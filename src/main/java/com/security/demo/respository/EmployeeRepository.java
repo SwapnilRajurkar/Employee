a9ba579d30bb93db1415cbc6f144ed030e92ce03
SWAPNIL RAJURKAR <138289682+SwapnilRajurkar@users.noreply.github.com>
2026-04-23 13:52:37 +0530

    Add files via upload
    commit changes

---
+++ src/main/java/com/security/demo/respository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.security.demo.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.security.demo.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
---
+++ src/main/java/com/security/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.security.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.security.demo.model.Employee;
+import com.security.demo.respository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository empRepo;
+
+	@Override
+	public void add(Employee emp) {
+		// TODO Auto-generated method stub
+		empRepo.save(emp); // insert
+	}
+
+	@Override
+	public List<Employee> display() {
+		// TODO Auto-generated method stub
+		return empRepo.findAll(); // Select *
+	}
+
+	@Override
+	public Employee update(Employee emp, Integer id) {
+		// TODO Auto-generated method stub
+		emp.setId(id);
+		return empRepo.save(emp); // Update
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		empRepo.deleteById(id); // delete
+	}
+
+}
